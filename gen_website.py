ee238e6e2ca597a022c7f44c6c571b8cd24075bd
nicholasdewaal <nicholasdewaal@gmail.com>
2020-07-20 18:21:19 -0500

    Make gen_website more pep8 compliant

--- gen_website.py
+++ gen_website.py
@@ -7,14 +7,14 @@ import matplotlib.pyplot as plt
 attribution = '</br> Fuente de los datos utilizados: <a href="https://www.datosabiertos.gob.pe/dataset/casos-positivos-por-covid-19-ministerio-de-salud-minsa"> Instituto Nacional de Salud y Centro Nacional de Epidemiologia, prevención y Control de Enfermedades – MINSA. </a>'
 attribution += '</br></br> <a href="https://github.com/nicholasdewaal/nicholasdewaal.github.io/blob/master/gen_website.py">Fuente del codigo</a> usado para generar este sitio web.</br></br></br></br>'
 
+
 def add_line(in_file, line):
     with open(in_file, 'a') as file:
         file.write('\n' + line)
 
 
 def fetch_png_list(in_path):
-    image_list = [x for x in os.listdir(in_path) if x[-3:] == "png"]
-    image_list.sort()
+    image_list = sorted([x for x in os.listdir(in_path) if x[-3:] == "png"])
     return image_list
 
 
@@ -39,23 +39,29 @@ def gen_plot(df_in, save_path):
         fig, ax = plt.subplots()
         # plt.xlabel("Fecha")
         # plt.ylabel("Casos Nuevos Detectados")
-        df_avg.plot(ax=ax, color="green", title="Casos Nuevos Detectados en " + save_path, legend=True)
+        df_avg.plot(
+            ax=ax,
+            color="green",
+            title="Casos Nuevos Detectados en " +
+            save_path,
+            legend=True)
         ax.bar(df_bars.index, df_bars.values)
         plt.tight_layout()
         x_axis = ax.axes.get_xaxis()
         y_axis = ax.axes.get_yaxis()
         x_label = x_axis.get_label()
-        y_label = y_axis.get_label()
+        # y_label = y_axis.get_label()
         x_label.set_visible(False)
         # y_label.set_visible(False)
         plt.savefig(save_path + ".png", dpi=120)
         plt.clf()
 
 
-df_pos = pd.read_csv("positivos_covid.csv", encoding = "ISO-8859-1")
+df_pos = pd.read_csv("positivos_covid.csv", encoding="ISO-8859-1")
 #df_dead = pd.read_csv("fallecidos_covid.csv", encoding = "ISO-8859-1")
 
-df_pos['FECHA_RESULTADO'] = pd.to_datetime(df_pos['FECHA_RESULTADO'], format="%d/%m/%Y")
+df_pos['FECHA_RESULTADO'] = pd.to_datetime(
+    df_pos['FECHA_RESULTADO'], format="%d/%m/%Y")
 
 df_pos.DISTRITO = df_pos.DISTRITO.str.replace('Ñ', 'N')
 df_pos.PROVINCIA = df_pos.PROVINCIA.str.replace('Ñ', 'N')
@@ -69,7 +75,7 @@ df_pos.PROVINCIA = df_pos.PROVINCIA.str.replace('Ó', 'O')
 
 try:
     arg = sys.argv[1]
-except:
+except BaseException:
     arg = ""
 
 if arg == "noimages":
@@ -88,7 +94,8 @@ for department in all_departments:
     gen_plot(df_department, department)
     containing_provinces = list(df_department['PROVINCIA'].unique())
     for province in containing_provinces:
-        # place provinces in department folder, with filename of province, also making folder with province name
+        # place provinces in department folder, with filename of province, also
+        # making folder with province name
         save_path = department + '/' + province
         make_dir(save_path)
         df_province = df_department[df_department.PROVINCIA == province]
@@ -111,16 +118,26 @@ for department in all_departments:
     department_images = fetch_png_list(department)
 
     add_line(department_link, "<html>\n    <head>\n    </head>\n    <body>")
-    add_line(department_link, "<h1>Casos de COVID-19 en " + department + " por Provincia</h1>")
-    add_line(department_link, "<h2>\n    Seleccione la foto de una Provincia para ver sus detalles por Distrito.\n</h2>")
-    add_line(department_link, "<h3>\n    <a href=../index.html>Regresar a casos por Departamento</a>\n</h3>")
+    add_line(
+        department_link,
+        "<h1>Casos de COVID-19 en " +
+        department +
+        " por Provincia</h1>")
+    add_line(
+        department_link,
+        "<h2>\n    Seleccione la foto de una Provincia para ver sus detalles por Distrito.\n</h2>")
+    add_line(
+        department_link,
+        "<h3>\n    <a href=../index.html>Regresar a casos por Departamento</a>\n</h3>")
 
     for image in department_images:
-        # Exclude generating html links of images of EN INVESTIGACION and those that are not png files.
+        # Exclude generating html links of images of EN INVESTIGACION and those
+        # that are not png files.
         if image[-3:] == "png" and image[:6] != "EN INV":
             # dist_image_num = len(os.listdir(department + '/' + province))
             # if dist_image_num > 0:
-            add_line(department_link, '        <a href="' + image[:-4] + '/'  + image[:-3] + 'html">')
+            add_line(department_link, '        <a href="' +
+                     image[:-4] + '/' + image[:-3] + 'html">')
             add_line(department_link, '            <img src="' + image + '">')
             # if dist_image_num > 0:
             add_line(department_link, r'        </a>')
@@ -136,12 +153,26 @@ for department in all_departments:
             remove_if_exists(province_link)
             os.mknod(province_link)
             province_images = fetch_png_list(department + '/' + province)
-            add_line(province_link, "<html>\n    <head>\n    </head>\n    <body>")
-            add_line(province_link, "<h1>Casos de COVID-19 en " + department +" / " + province + " por Distrito</h1>")
-            add_line(province_link, "<h3>\n    <a href=../../index.html>Regresar a casos por Departamento</a>\n</h3>")
+            add_line(
+                province_link,
+                "<html>\n    <head>\n    </head>\n    <body>")
+            add_line(
+                province_link,
+                "<h1>Casos de COVID-19 en " +
+                department +
+                " / " +
+                province +
+                " por Distrito</h1>")
+            add_line(
+                province_link,
+                "<h3>\n    <a href=../../index.html>Regresar a casos por Departamento</a>\n</h3>")
             for image in province_images:
                 if image[-3:] == "png" and image[:6] != "EN INV":
-                    add_line(province_link, '        <img src="' + image + '">')
+                    add_line(
+                        province_link,
+                        '        <img src="' +
+                        image +
+                        '">')
 
             add_line(province_link, attribution)
             add_line(province_link, "</body>\n</html>")
